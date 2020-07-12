8749f6dd8b80181d1dd8812763273514f0bb035d
Bekir Eldem <bekir.eldem@gmail.com>
2020-07-12 13:54:57 +0000

    poc-1

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('main_static_2');
-});
+// Route::get('/', function () {
+//     return view('main_static_2');
+// });
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'ThreadController@index');
 
 Route::resource('threads', 'ThreadController');
